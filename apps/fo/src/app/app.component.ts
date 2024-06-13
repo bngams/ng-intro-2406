e6104a74b6f6ff80701064497d4f3d04e2b53887
bngams <n.boris@devidia.net>
2024-06-13 09:35:00 +0000

    template syntax

--- apps/fo/src/app/app.component.ts
+++ apps/fo/src/app/app.component.ts
@@ -1,14 +1,35 @@
+import { CommonModule, DatePipe, LowerCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CoreComponent } from 'core';
+import { ExponentialStrengthPipe } from './pipes/exponential-strength.pipe';
+
+const libs = [
+  RouterOutlet,
+  CommonModule, // ngIf, etc..
+  CoreComponent,
+];
+
+const pipes = [
+  UpperCasePipe,
+  LowerCasePipe,
+  DatePipe,
+  ExponentialStrengthPipe
+] 
 
 @Component({
   selector: 'fo-root',
   standalone: true,
-  imports: [RouterOutlet, CoreComponent],
+  imports: [...libs, ...pipes],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'fo';
+  today = new Date();
+  dateFormat = 'short';
+
+  myMethod() {
+    alert('Hello !!');
+  }
 }
--- libs/core/src/public-api.ts
+++ libs/core/src/public-api.ts
@@ -1,6 +1,4 @@
 /*
  * Public API Surface of core
  */
-
-export * from './lib/core.service';
 export * from './lib/core.component';
